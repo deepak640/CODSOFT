c0a76cd9d7502070eeba11f23feedadfdac5aa3d
deepak640 <ayushdeepnegi@gmail.com>
2024-07-18 21:54:37 +0530

    updated UI

--- taskTracker/src/components/List.jsx
+++ taskTracker/src/components/List.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-
+import {DeleteOutlined} from "@ant-design/icons"
 const List = () => {
   return (
     <>
       <ul className="tasks">
         {[...Array(5)].map((_, index) => {
-          return <li className="items" key={index}>hello</li>;
+          return (
+            <li className="items" key={index}>
+              <p>items</p>
+              <DeleteOutlined className="icon" />
+            </li>
+          );
         })}
       </ul>
     </>
