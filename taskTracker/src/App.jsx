10dd7ca321e3d21133a5907f6f07c59849505106
deepak640 <ayushdeepnegi@gmail.com>
2024-07-19 17:27:08 +0530

    updated functionality

--- taskTracker/src/App.jsx
+++ taskTracker/src/App.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
-import List from './components/List'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import List from "./components/List";
+import "./App.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 const App = () => {
+  const [input, setInput] = useState("");
+  const [task, setTask] = useState("");
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      setTask(input);
+      setInput("");
+    }
+  };
+ useEffect(() => {
+   AOS.init();
+ }, []);
   return (
-    <div className='container'>
-    <h1 className="heading">Task Tracker</h1>
-    <div className='taskContainer'>
-    <input type='text' className="search" placeholder='Note'/>
-    <List/>
+    <div className="container">
+      <h1 className="heading">Task Tracker</h1>
+      <div className="taskContainer">
+        <input
+          type="text"
+          className="search"
+          placeholder="Note"
+          name="input"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+        <List task={task} />
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
--- taskTracker/src/components/List.jsx
+++ taskTracker/src/components/List.jsx
@@ -1,20 +1,46 @@
-import React from "react";
-import {DeleteOutlined} from "@ant-design/icons"
-const List = () => {
+import React, { useState, useEffect } from "react";
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
+
+const List = ({ task }) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    if (task) {
+      setData((prev) => [...prev, task]);
+    }
+  }, [task]);
+
+  const handleDeleteTodo = (item) => {
+    setData((prev) => prev.filter((d) => d !== item));
+  };
+
   return (
     <>
-      <ul className="tasks">
-        {[...Array(5)].map((_, index) => {
-          return (
-            <li className="items" key={index}>
-              <p>items</p>
-              <DeleteOutlined className="icon" />
-            </li>
-          );
-        })}
-      </ul>
+      {data.length ? (
+        <ul className="tasks">
+          {data.map((item, index) => {
+            return (
+              <li className="items" key={index} data-aos="zoom-in">
+              <div>
+                <p>{item}</p>
+                <p>date</p>
+              </div>
+                <div>
+                <EditOutlined style={{color:'blue'}}  className="icon"/>
+                <DeleteOutlined style={{color:'red'}} className="icon"
+                  onClick={() => handleDeleteTodo(item)}
+                />
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <p className="emptynote">add your data here</p>
+      )}
     </>
   );
 };
 
 export default List;
+
