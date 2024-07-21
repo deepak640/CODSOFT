2459bf95071f441b7c3f9b3f9ecb82ebc513bbf4
deepak640 <ayushdeepnegi@gmail.com>
2024-07-21 20:38:20 +0530

    bug fix

--- taskTracker/src/components/CompletedTask.jsx
+++ taskTracker/src/components/CompletedTask.jsx
@@ -1,8 +1,12 @@
 import { DeleteOutlined } from "@ant-design/icons";
+import { useEffect } from "react";
 const CompletedTask = ({ data, setData }) => {
   const handleDelete = (task) => {
     setData((prev) => prev.filter(({ topic }) => topic !== task));
   };
+  useEffect(() => {
+    localStorage.setItem("tasksCompleted",JSON.stringify(data));
+  });
   return (
     <div className="tasks completed-task-container">
       <h1 align="center">Completed tasks</h1>
